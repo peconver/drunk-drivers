f7c112b127be0ec78ccf705bf894bb0d338b3245
Goldmage <Venousek98@gmail.com>
2020-12-10 15:38:04 +0100

    Update wrap_humanoid.py
    added hyper-parameters, more enviroments and tensorboard

--- wrap_humanoid.py
+++ wrap_humanoid.py
@@ -1,4 +1,4 @@
-from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, SubprocVecEnv
 from stable_baselines3 import A2C
 
 from humanoid import HumanoidBulletEnv
@@ -6,17 +6,19 @@ from humanoid import HumanoidBulletEnv
 
 if __name__ == "__main__":
     env_max_steps = 1500
-    learn_epochs = 500
+    learn_epochs = 100
     learn_total_steps = learn_epochs * env_max_steps
     learn_verbose = 0
 
     train = False
     
     if train:
-        env = DummyVecEnv([lambda: HumanoidBulletEnv(animate=False, max_steps=env_max_steps)])
-        env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
+        # change the number in range if you see errors, the number depends on power of your computer
+        env = SubprocVecEnv([lambda: HumanoidBulletEnv(animate=False, max_steps=env_max_steps) for i in range(17)]) 
+        env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=8., epsilon=1e-8)
+        #env = HumanoidBulletEnv(animate=False, max_steps=env_max_steps)
 
-        model = A2C('MlpPolicy', env, policy_kwargs=dict(net_arch=[256, 256]), verbose=learn_verbose)
+        model = A2C('MlpPolicy', env, tensorboard_log="./a2c_humanoid_tensorboard/", use_sde=True, gae_lambda=0.9, vf_coef=0.4, ent_coef=0.001, n_steps=8, learning_rate=8e-4, policy_kwargs=dict(net_arch=[128, 128], log_std_init=-2, ortho_init=False), verbose=learn_verbose)
         model.learn(learn_total_steps)
         model.save("first_try")
 
@@ -30,8 +32,12 @@ if __name__ == "__main__":
         env.training = False
         env.norm_reward = False
 
+        #env = HumanoidBulletEnv(animate=True, max_steps=env_max_steps)
+        #model = A2C.load("first_try")
         obs = env.reset()
         for i in range(env_max_steps):
             action, _states = model.predict(obs)
             obs, rewards, dones, info = env.step(action)
+            print(rewards)
+            #env.render()
         env.close()
