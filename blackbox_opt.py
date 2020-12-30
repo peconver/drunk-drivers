114891b62bf634d3690288763869b9bc3d03ed55
veronika <peconver@fel.cvut.cz>
2020-12-30 09:34:46 +0100

    Blackbox_opt fixed, added best weights from trainig 50 000 000 steps

--- blackbox_opt.py
+++ blackbox_opt.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 from humanoid import HumanoidBulletEnv
+total_steps = 0
 
 class BlackBoxOpt():
     def __init__(self, obs_dim, act_dim):
@@ -8,15 +9,19 @@ class BlackBoxOpt():
         # The simplest policy to implement would be a simple affine mapping y = xw+b, where x is the input,
         # w is the weight matrix and b is the biasget_params
         rng = np.random.default_rng()
-        self.policy = rng.uniform(-0.1, 0.1, [98,20])
-        self.b = rng.uniform(-0.1,0.1, [98,1])
+        self.policy = rng.uniform(-0.1, 0.1, [obs_dim,act_dim])
+        self.b = rng.uniform(-0.1,0.1, [act_dim,1])
+        self.act_dim = act_dim
+        self.obs_dim = obs_dim
+        self.total_steps = 0
+        # print(act_dim, obs_dim)
         # TODO change initialization of weights in main if train
 
     def set_params(self, w):
-       # print('Shape!!!!', len(w))
-        self.b = w[1666:1683]
+        # print('Shape!!!!', len(w))
+        self.b = w[self.act_dim*self.obs_dim:(self.act_dim*self.obs_dim+self.act_dim)]
        # print('b!!!', self.b)
-        self.policy = np.reshape(w[0:1666], (17,98))
+        self.policy = np.reshape(w[0:self.act_dim*self.obs_dim], (self.act_dim,self.obs_dim))
         # This function takes in a list w and maps it to your policy parameters.
         # The simplest way is to probably make an array out of the w vector and reshape it appropriately TODO: policy = res haped(w)
     def get_params(self):
@@ -37,6 +42,8 @@ def f_wrapper(env, policy):
         policy.set_params(w)
 
         while not done:
+            # log total steps count
+            policy.total_steps = policy.total_steps+1
 
             # Get action from policy
             act = policy.forward(obs)
@@ -49,7 +56,7 @@ def f_wrapper(env, policy):
 
     return f
 
-def my_opt(f, w_init, iters):
+def my_opt(policy, f, w_init, iters):
     # Your optimization algorithm. Takes in an evaluation function f, and initial solution guess w_init and returns
     # parameters w_best which 'solve' the problem to some degree.
     # vyzkouset f() na ruzna w a vratit tu nejlepsi
@@ -58,10 +65,14 @@ def my_opt(f, w_init, iters):
     r_best = f(w_best)
     policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'learned/humanoid_bbx_')
     
-    # model will train forever and trained weights are stored every 252 iterations
+    # model will train 50 000 000 steps and trained weights are stored every 5000 epoch
     print()
-    while(1):
-        for i in range(252):
+    j = 0
+    epoch  = 0
+    store = 0
+    len_w = len(w)
+    while(policy.total_steps<50000000):
+        for i in range(len_w):
             w[i] = w[i] + 0.05
             r = f(w)
             if(r and r > r_best):
@@ -76,12 +87,23 @@ def my_opt(f, w_init, iters):
                     w_best = w.copy()
                 else:
                     w = w_best.copy()
-            # print('Progress: ', round(i/252*100), '%', end='\r') 
+            # every 5000 epoch store best weights
+            if(store==5000):
+                store = 0
+                np.save(policy_path + str(round(r_best)) + '.npy', w_best)
+            # after every epoch store reward and number of steps so far
+            print(policy.total_steps, r)
+            # every 50 epoch store best reward
+            # if (i+1)%50 is 0:
+            #        print(epoch, w) 
+            epoch = epoch + 1
+            store = store + 1
+        # print('Progress: ', round(i/252*100), '%', end='\r') 
         # print("r_best: {r_best, w_best}")
-        print("r_best:", r_best)
-        np.save(policy_path + str(round(r_best)) + '.npy', w_best)
-        
-        
+        # print("r_best:", r_best)
+        # np.save(policy_path + str(round(r_best)) + '.npy', w_best)
+    print('END')
+    np.save(policy_path + str(round(r_best)) + '.npy', w_best)    
     return w_best, r_best
 
 def test(w_best, max_steps=70, animate=False):
@@ -102,10 +124,10 @@ def test(w_best, max_steps=70, animate=False):
     return r_avg / eval_iters
 
 if __name__ == "__main__":
-    train = True
-    # policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'learned/humanoid_bbx.npy')
+    train = False
+    policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'learned/humanoid_bbx.npy')
     max_steps = 1000
-    N_training_iters = ...
+    N_training_iters =50000
     w_best = None
     
     if train:
@@ -120,12 +142,12 @@ if __name__ == "__main__":
         # Initial guess of solution
         # w_init = np.random.rand(1, env.act_dim*21)e
         rng = np.random.default_rng()
-        w_init = rng.uniform(-0.1, 0.1, [98,21])
+        w_init = rng.uniform(-0.1, 0.1, [env.obs_dim+1,env.act_dim])
         w_init = w_init.flatten()
         # Perform optimization
-        w_best, r_best = my_opt(f, w_init, N_training_iters)
+        w_best, r_best = my_opt(policy,f, w_init, N_training_iters)
 
-        print(f"r_best: {r_best}")
+        print("r_best: {r_best}")
 
         # Save policy
         np.save(policy_path, w_best)
@@ -133,7 +155,7 @@ if __name__ == "__main__":
 
     if not train:
         # WARN: Change policy path, look into dir 'learned' and choose desired model
-        policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'learned/humanoid_bbx_137.npy')
+        policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'learned/humanoid_bbx_174.npy')
         w_best = np.load(policy_path)
     print(f"Avg test rew: {test(w_best, max_steps=max_steps, animate=not train)}")
 
